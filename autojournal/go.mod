b568c85f3c9b99c1f1566b88aacd6a6ba39f12cd
whylovely <viner05@list.ru>
2026-07-14 15:09:45 +0300

    Написание первого шага 3 фазы (подключение БД)

--- autojournal/go.mod
+++ autojournal/go.mod
@@ -12,6 +12,7 @@ require (
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/gorilla/websocket v1.5.3 // indirect
 	github.com/jchv/go-winloader v0.0.0-20210711035445-715c2860da7e // indirect
+	github.com/jmoiron/sqlx v1.4.0 // indirect
 	github.com/labstack/echo/v4 v4.13.3 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/leaanthony/go-ansi-parser v1.6.1 // indirect
--- autojournal/internal/storage/sqlite.go
+++ autojournal/internal/storage/sqlite.go
@@ -1,1 +1,42 @@
 package storage
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/jmoiron/sqlx"
+)
+
+func OpenSQLite() (*sqlx.DB, error) {
+	configDir, err := os.UserConfigDir()
+	if err != nil {
+		return nil, fmt.Errorf("get user config dir: %w", err)
+	}
+
+	appDir := filepath.Join(configDir, "autojournal")
+	if err := os.MkdirAll(appDir, 0o755); err != nil {
+		return nil, fmt.Errorf("create app dir: %w", err)
+	}
+
+	dbPath := filepath.Join(appDir, "data.db")
+
+	db, err := sqlx.Open("sqlite3", dbPath)
+	if err != nil {
+		return nil, fmt.Errorf("open sqlite: %w", err)
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, fmt.Errorf("ping sqlite: %w", err)
+	}
+
+	if _, err := db.Exec(`PRAGME journal_mode=WAL;`); err != nil {
+		return nil, fmt.Errorf("enable wal: %w", err)
+	}
+
+	if _, err := db.Exec(`PRAGMA foreign_keys=ON;`); err != nil {
+		return nil, fmt.Errorf("enable foreign keys: %w", err)
+	}
+
+	return db, nil
+}
